b9e4d8d0fbeac64ff3fe1d1f1ea76fe956b1e626
4aac <management.asher@gmail.com>
2025-08-19 21:22:24 +0200

    feat: added eleven labs text to speech

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
 from user_speech_processing import speech_to_text, text_processing
-from response_processing import response
+from response_processing import response, vocal_response
 
-def main():
-    user = speech_to_text()
-    user_processed = text_processing(user)
-    answer = response(user_processed)
-    return user_processed, answer
 
-convo = main()
-print(f"Asier: {convo[0]}")
-print(f"AI: {convo[1]}")
+vocal_response()
+
+
+# def main():
+#     user = speech_to_text()
+#     user_processed = text_processing(user)
+#     answer = response(user_processed)
+#     return user_processed, answer
+
+# convo = main()
+# print(f"Asier: {convo[0]}")
+# print(f"AI: {convo[1]}")
 
--- app/response_processing.py
+++ app/response_processing.py
@@ -1,9 +1,16 @@
-from user_speech_processing import text_processing
+from user_speech_processing import text_processing, speech_to_text
 
 from PRIVATE_KEYS import OPENAI_API_KEY
 from openai import OpenAI
 
+from PRIVATE_KEYS import ELEVENLABS_API_KEY
+from elevenlabs.client import ElevenLabs
+from elevenlabs import play
+
+
 def response(context: str) -> str:
+    """
+    """
     client = OpenAI(api_key=OPENAI_API_KEY)
     chat_response = client.chat.completions.create(
         model="gpt-4o",
@@ -13,3 +20,22 @@ def response(context: str) -> str:
         ]
     )
     return chat_response.choices[0].message.content
+
+
+def vocal_response() -> None:
+    """
+    """
+    elevenlabs = ElevenLabs(
+    api_key=ELEVENLABS_API_KEY,
+    )
+
+    audio = elevenlabs.text_to_speech.convert(
+        text=response(text_processing(speech_to_text())),
+        voice_id="JBFqnCBsd6RMkjVDRZzb",
+        model_id="eleven_multilingual_v2",
+        output_format="mp3_44100_128",
+    )
+
+    play(audio)
+
+
